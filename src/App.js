e0955ee41b91edca835895a21ab22fef4e3f3740
Suman Bokkasam <bokks@pegasystems.com>
2020-11-16 12:38:20 +0530

    Simple app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useContext } from 'react';
+import { Button, Modal, ModalManagerContext, Text } from '@pega/cosmos-react-core';
 
+
+const ModalExample = () => {
+  const MyModal = () => {
+    return (
+      <Modal heading='This is the Modal heading'>
+        <Text>This is the content of the Modal.</Text>
+      </Modal>
+    );
+  };
+  const ModalButton = () => {
+    const { create } = useContext(ModalManagerContext);
+    return (
+      <Button
+        onClick={() => {
+          create(MyModal);
+        }}
+      >
+        Open Modal
+      </Button>
+    );
+  };
+  return <ModalButton />;
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ModalExample />
     </div>
   );
 }
